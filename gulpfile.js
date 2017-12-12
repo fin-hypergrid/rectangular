da7517823973c847ddb18c4ba4c0d25f8e55f52f
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-12 10:01:55 -0500

    versioned build

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    $$   = require('gulp-load-plugins')();
+    manifest = require('./package.json'),
+    make = require('fin-hypergrid-client-module-maker'),
+    options = Object.assign({
+        name: manifest.name,
+        version: manifest.version,
+        tasks: ['lint', 'test', 'build']
+    }, manifest.wrapper);
 
-var runSequence = require('run-sequence'),
-    browserSync = require('browser-sync').create(),
-    exec        = require('child_process').exec,
-    path        = require('path');
-
-var name     = 'rectangular',
-    srcDir   = './src/',
-    testDir  = './test/',
-    buildDir = './build';
-
-//  //  //  //  //  //  //  //  //  //  //  //
-
-gulp.task('lint', lint);
-gulp.task('test', test);
-gulp.task('doc', doc);
-gulp.task('browserify', function(callback) {
-    browserify();
-    browserifyMin();
-    callback();
-});
-
-gulp.task('build', function(callback) {
-    clearBashScreen();
-    runSequence(
-        'lint',
-        'test',
-        'doc',
-        'browserify',
-        callback
-    );
-});
-
-gulp.task('watch', function () {
-    gulp.watch([srcDir + '**', testDir + '**'], ['build'])
-        .on('change', function(event) {
-            browserSync.reload();
-        });
-});
-
-gulp.task('default', ['build', 'watch'], browserSyncLaunchServer);
-
-//  //  //  //  //  //  //  //  //  //  //  //
-
-function lint() {
-    return gulp.src(srcDir + 'index.js')
-        .pipe($$.excludeGitignore())
-        .pipe($$.eslint())
-        .pipe($$.eslint.format())
-        .pipe($$.eslint.failAfterError());
-}
-
-function test(cb) {
-    return gulp.src(testDir + 'index.js')
-        .pipe($$.mocha({reporter: 'spec'}));
-}
-
-function browserify() {
-    return gulp.src(srcDir + 'browserify_root.js')
-        .pipe($$.browserify({
-            //insertGlobals : true,
-            debug : true
-        }))
-        //.pipe($$.sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here:
-
-        .on('error', $$.util.log)
-
-        .pipe($$.rename(name + '.js'))
-        .pipe(gulp.dest(buildDir)); // outputs to ./build/rectangular.js for githup.io publish
-}
-
-function browserifyMin() {
-    return gulp.src(srcDir + 'browserify_root.js')
-        .pipe($$.browserify())
-        .pipe($$.uglify())
-        .pipe($$.rename(name + '.min.js'))
-        .pipe(gulp.dest(buildDir)); // outputs to ./build/rectangular.min.js for githup.io publish
-}
-
-function doc(cb) {
-    exec(path.resolve('jsdoc.sh'), function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-}
-
-function browserSyncLaunchServer() {
-    browserSync.init({
-        server: {
-            // Serve up our build folder
-            baseDir: buildDir,
-            index: "demo.html"
-        },
-        port: 5006
-    });
-}
-
-function clearBashScreen() {
-    var ESC = '\x1B';
-    console.log(ESC + 'c'); // (VT-100 escape sequence)
-}
+make(gulp, options);
--- src/browserify_root.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-/* eslint-env node, browser */
-
-if (!window.rectangular) {
-    window.rectangular = require('./');
-}
